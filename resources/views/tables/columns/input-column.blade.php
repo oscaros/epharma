e887c71968447a7633597c001b5bbdc8b13a38bf
wdsamuel <wogutedavidsamuel@gmail.com>
2024-04-27 16:09:10 +0300

    db updates

--- resources/views/tables/columns/input-column.blade.php
+++ resources/views/tables/columns/input-column.blade.php
@@ -7,8 +7,8 @@
         class="form-input block w-full mt-1 rounded-md focus:ring-blue-500 focus:border-blue-500"
         value="" 
         data-product-id="{{ $getRecord()->id }}"
-        data-product-name="{{ $getRecord()->name }}" 
-        data-product-price="{{ $getRecord()->price }}" 
+        data-product-name="{{ $getRecord()->ProductName }}" 
+        data-product-price="{{ $getRecord()->Price }}" 
         onchange="updateCart(this)" 
     />
 
