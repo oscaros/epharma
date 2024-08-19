f1ab2b96760712018d34315cf0db51e70cbd7cf8
wdsamuel <wogutedavidsamuel@gmail.com>
2024-08-20 00:00:59 +0300

    .

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -17,19 +17,27 @@
             <div class="flex flex-col items-center justify-center space-y-4">
                  <p class="text-lg"><strong>Today's Serial Number: </strong> {{ $customer->NewVisitNumber }}</p>
                  
-                @if($customer->qr_code_path)
+                {{-- @if($customer->qr_code_path)
                     <div class="mb-4" id="qrcode">
                         <h3 class="text-2xl font-semibold mb-2 text-gray-800">QR Code:</h3>
                         <div class="p-4 bg-gray-100 rounded-lg">
                             {{-- <img src="{{ asset('storage/'.$customer->qr_code_path) }}" alt="QR Code" class="w-48 h-48" onerror="this.onerror=null;this.src='{{ asset('images/01.png') }}';"> --}}
-                            {{-- <img src="{{ url('storage/' . $customer->qr_code_path) }}" alt="QR Code" class="w-48 h-48"> --}}
+                            {{-- <img src="{{ url('storage/' . $customer->qr_code_path) }}" alt="QR Code" class="w-48 h-48"> 
 
                         </div>
                     </div>
                 @else
                     <p>QR Code not available.</p>
-                @endif
+                @endif --}}
+                
+                
+                <div id="qrcode" class="items-center justify-center space-y-4"></div> 
+
+
+
             </div>
+
+            
             
             
         </div>
